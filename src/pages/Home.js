4eb947ef7c9d4a8290acd4a2f72e8b1f1df406ad
praneethrms04 <praneethrms04@gmail.com>
2022-11-22 13:11:48 +0530

    final commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,13 @@
 import Navbar from "../components/Navbar";
-// import Navbar1 from "../components/Navbar1"
-// import Footer from "../components/Footer";
-// import HomeIntro from "../components/HomeIntro";
+import Footer from "../components/Footer";
+import HomeIntro from "../components/HomeIntro";
 
 function Home() {
   return (
     <>
-    {/* <Navbar1 /> */}
       <Navbar />
-      {/* <HomeIntro /> */}
-      {/* <Footer /> */}
+      <HomeIntro />
+      <Footer />
     </>
   );
 }
